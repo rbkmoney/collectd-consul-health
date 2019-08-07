bd5c52792db7b4c3f7b3fad192c2f356461f009d
Andrew Mayorov <a.mayorov@rbkmoney.com>
2019-08-07 11:16:55 +0300

    Implement collectd plugin + tests

---
+++ test/test_configure.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+import mock
+import unittest
+import re
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
+sys.modules['collectd'] = mock.MagicMock()
+import consul_health_plugin
+
+
+class TestConfiure(unittest.TestCase):
+
+    @mock.patch('consul_health_plugin.ConsulPlugin')
+    def test_default_config(self, mock_plugin):
+
+        mock_conf = mock.Mock()
+        mock_conf.children = []
+
+        expected_conf = {'api_host': 'localhost',
+                         'api_port': 8500,
+                         'api_protocol': 'http',
+                         'acl_token': None,
+                         'ssl_certs': {'ca_cert': None, 'client_cert': None,
+                                       'client_key': None},
+                         'debug': False}
+        consul_health_plugin.configure_callback(mock_conf)
+        args, kwargs = mock_plugin.call_args
+        for k, v in args[0].items():
+            self.assertIn(k, expected_conf)
+            self.assertEqual(v, expected_conf[k])
+
+    @mock.patch('consul_health_plugin.ConsulPlugin')
+    def test_all_config(self, mock_plugin):
+        mock_plugin.read = mock.MagicMock()
+        mock_plugin.shutdown = mock.MagicMock()
+
+        mock_conf = mock.Mock()
+
+        expected_host = '10.2.5.60'
+        mock_host = _build_mock_config_child('ApiHost', expected_host)
+
+        expected_port = 8080
+        mock_port = _build_mock_config_child('ApiPort', str(expected_port))
+
+        expected_protocol = 'https'
+        mock_protocol = _build_mock_config_child('ApiProtocol',
+                                                 expected_protocol)
+
+        expected_acl_token = 'acl_token'
+        mock_acl_token = _build_mock_config_child('AclToken',
+                                                  expected_acl_token)
+
+        expected_ca_cert = '/path/to/ca/cert'
+        mock_ca_cert = _build_mock_config_child('CaCertificate',
+                                                expected_ca_cert)
+
+        expected_client_cert = '/path/to/clien/cert'
+        mock_client_cert = _build_mock_config_child('ClientCertificate',
+                                                    expected_client_cert)
+
+        expected_client_key = '/path/to/client/key'
+        mock_client_key = _build_mock_config_child('ClientKey',
+                                                   expected_client_key)
+
+        expected_ssl_certs = {'ca_cert': expected_ca_cert,
+                              'client_cert': expected_client_cert,
+                              'client_key': expected_client_key}
+
+        expected_debug = False
+        mock_debug = _build_mock_config_child('Debug', expected_debug)
+
+        mock_conf.children = [mock_host,
+                              mock_port,
+                              mock_protocol,
+                              mock_acl_token,
+                              mock_ca_cert,
+                              mock_client_cert,
+                              mock_client_key,
+                              mock_debug]
+
+        expected_conf = {'api_host': expected_host,
+                         'api_port': expected_port,
+                         'api_protocol': expected_protocol,
+                         'acl_token': expected_acl_token,
+                         'ssl_certs': expected_ssl_certs,
+                         'debug': expected_debug}
+
+        consul_health_plugin.configure_callback(mock_conf)
+        args, kwargs = mock_plugin.call_args
+        for k, v in args[0].items():
+            self.assertEqual(v, expected_conf[k])
+
+        consul_health_plugin.collectd.register_read.assert_called_once()
+        consul_health_plugin.collectd.register_read.assert_called_with(
+            mock_plugin().read, name='consul-health.{}:{}'.format(expected_host,
+                                                    expected_port))
+
+
+def _build_mock_config_child(key, value):
+    mock_config_child = mock.Mock()
+    mock_config_child.key = key
+    mock_config_child.values = [value]
+    return mock_config_child
---
+++ test/test_consul_agent.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+import mock
+import unittest
+import sys
+import json
+import os
+import urllib.request
+from urllib.error import HTTPError
+from urllib.error import URLError
+sys.path.insert(0, os.path.dirname(__file__))
+sys.modules['collectd'] = mock.Mock()
+
+from consul_health_plugin import ConsulAgent
+
+
+class MockHTTPSHandler(object):
+
+    def __init__(self, ca_cert, client_cert, client_key):
+        self.ca_cert = ca_cert
+        self.client_cert = client_cert
+        self.client_key = client_key
+
+
+def sample_response(path):
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file = dir_path + '/resources' + path
+    with open(file, 'r') as data:
+        return json.load(data)
+
+
+class ConsulAgentTest(unittest.TestCase):
+    def setUp(self):
+        self.maxDiff = None
+        self.base_url = 'http://example.com:8888/v1'
+        self.api_host = 'example.com'
+        self.api_port = '8888'
+        self.api_protocol = 'http'
+        self.acl_token = 'acl_token'
+        self.ssl_certs = {'ca_cert': None,
+                          'client_cert': None,
+                          'client_key': None}
+        self.agent = ConsulAgent(self.api_host, self.api_port,
+                                 self.api_protocol, self.acl_token,
+                                 self.ssl_certs)
+        self.agent.config = sample_response('/agent/self')
+
+    @mock.patch('urllib.request.build_opener')
+    def test_return_json_on_ok(self, mock_urllib_opener):
+
+        mock_response = mock.Mock()
+        mock_response.code = 200
+        mock_response.read.return_value = '{"foo": "bar", "bat": "baz"}'
+
+        mock_urllib_opener().open.return_value = mock_response
+
+        url = 'http://example.com:8500'
+        actual_response = self.agent._send_request(url)
+        expected_response = {"foo": "bar", "bat": "baz"}
+
+        expected_header = [('X-Consul-Token', self.agent.acl_token)]
+        self.assertEqual(mock_urllib_opener.return_value.addheaders,
+                         expected_header)
+        mock_urllib_opener().open.assert_called_with(url)
+
+        self.assertDictEqual(expected_response, actual_response)
+
+    @mock.patch('urllib.request.build_opener')
+    def test_return_none_on_exception(self, mock_urllib_opener):
+
+        mock_urllib_opener.side_effect = HTTPError(*[None] * 5)
+        self.assertIsNone(self.agent._send_request('http://example.com:8500'))
+
+        mock_urllib_opener.side_effect = URLError('Mock URLError')
+        self.assertIsNone(self.agent._send_request('http://exampl.com:8500'))
+
+    @mock.patch('consul_health_plugin.ConsulAgent._send_request')
+    def test_get_local_config(self, mock_send_request):
+        expected_url = '{0}/agent/self'.format(self.base_url)
+        self.agent.get_local_config()
+        mock_send_request.assert_called_with(expected_url)
+
+    @mock.patch('consul_health_plugin.ConsulAgent._send_request')
+    def test_get_health_checks(self, mock_send_request):
+        node = 'mhost1.dummy.net'
+        expected_url = '{0}/health/node/{1}'.format(self.base_url, node)
+        self.agent.get_health_checks(node)
+        mock_send_request.assert_called_with(expected_url)
+
+    @mock.patch('consul_health_plugin.ConsulAgent.get_health_checks',
+                return_value=sample_response('/health/node/mhost1.dummy.net'))
+    def test_get_health_check_stats(self, mock_call):
+        expected_response = {'service': {
+                                 'leftpad': {'passing': 1,
+                                             'warning': 0,
+                                             'critical': 1},
+                                 'devnull': {'passing': 0,
+                                             'warning': 1,
+                                             'critical': 0}
+                             },
+                             'node': {'passing': 1,
+                                      'warning': 0,
+                                      'critical': 0}}
+
+        actual_response = self.agent.get_health_check_stats()
+        self.assertEqual(expected_response, actual_response)
