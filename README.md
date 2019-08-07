57d2f61178e4e999c5bda2b07da08e1db2411d2a
Andrew Mayorov <a.mayorov@rbkmoney.com>
2019-08-07 14:13:32 +0300

    Implement collectd plugin (#1)

---
+++ README.md
@@ -0,0 +1,60 @@
+# collectd Consul Health Plugin
+
+A consul collectd plugin which users can use to send states of health checks for every service on a node. State is obtained from the cluster-wide view, not the local view. Provides metrics of the following form:
+
+```
+gauge service.{service}.{status}
+gauge node.{status}
+```
+
+...where `status` is either `passing`, `warning` or `critical`.
+
+Almost entirely "inspired" by [collectd-consul](https://github.com/signalfx/collectd-consul).
+
+## Installation
+
+1. Checkout this repository somewhere on your system accessible by collectd. The suggested location is `/usr/share/collectd/`
+1. Configure the plugin (see below)
+1. Restart collectd
+
+## Requirements
+
+* collectd 4.9 or later (for the Python plugin)
+* Python 3.6 or later
+* Consul 0.7.0 or later
+
+## Configuration
+
+Next configure the collectd-consul plugin by using the below given example configuration file as a guide, provide values for the configuration options listed in the table that make sense for your environment.
+
+**Configuration Option** | **Description** | **Default Value**
+:------------------------|:----------------|:------------------
+ApiHost	| IP address or DNS to which the Consul HTTP/HTTPS server binds to on the instance to be monitored | `localhost`
+ApiPort |	Port to which the Consul HTTP/HTTPS server binds to on the instance to be monitored |	`8500`
+ApiProtocol | Possible values - *http* or *https*	| `http`
+AclToken | Consul ACL token. | None
+CaCertificate | If Consul server has https enabled for the API, provide the path to the CA Certificate. | None
+ClientCertificate | If client-side authentication is enabled, provide the path to the certificate file. | None
+ClientKey | If client-side authentication is enabled, provide the path to the key file. | None
+Debug | Enable debug level logging. `true` / `false` | `false`
+Verbose | Escalate debug level logging up to info. `true` / `false` | `false`
+
+Note that multiple Consul instances can be configured in the same file.
+
+```
+LoadPlugin python
+
+<Plugin python>
+  ModulePath "/var/share/collectd/consul-health"
+
+  Import consul_health_plugin
+  <Module consul_health_plugin>
+    ApiHost "localhost"
+    ApiPort 8500
+    ApiProtocol "http"
+    Debug true
+  </Module>
+  ...
+
+</Plugin>
+```
---
+++ test/test_consul_plugin.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+import os
+import sys
+import json
+import unittest
+from mock import Mock, MagicMock, patch
+sys.path.insert(0, os.path.dirname(__file__))
+# Mock out the collectd module
+sys.modules['collectd'] = Mock()
+import consul_health_plugin # noqa
+from consul_health_plugin import ConsulAgent, MetricRecord # noqa
+
+
+class MockMetricSink(object):
+    def __init__(self):
+        self.captured_records = []
+
+    def emit(self, metric_record):
+        self.captured_records.append(metric_record)
+
+
+class TestConsulPlugin(unittest.TestCase):
+
+    @patch('consul_health_plugin.MetricSink')
+    def setUp(self, mock_sink):
+        self.maxDiff = None
+        mock_consul_agent = self._build_mock_consul_agent()
+        mock_sink.side_effect = MockMetricSink
+        self.plugin_conf = {'api_host': 'example',
+                            'api_port': 8500,
+                            'api_protocol': 'http',
+                            'acl_token': None,
+                            'ssl_certs': {'ca_cert': None,
+                                          'client_cert': None,
+                                          'client_key': None},
+                            'debug': False
+                            }
+        with patch('consul_health_plugin.ConsulAgent') as m_agent:
+            m_agent.return_value = mock_consul_agent
+            self.plugin = consul_health_plugin.ConsulPlugin(self.plugin_conf)
+
+    def test_fetch_health_checks(self):
+
+        expected_records = []
+
+        expected_records.append(MetricRecord(
+                                'service.leftpad.passing',
+                                'gauge',
+                                1))
+        expected_records.append(MetricRecord(
+                                'service.leftpad.warning',
+                                'gauge',
+                                0))
+        expected_records.append(MetricRecord(
+                                'service.leftpad.critical',
+                                'gauge',
+                                1))
+
+        expected_records.append(MetricRecord(
+                                'service.devnull.passing',
+                                'gauge',
+                                0))
+        expected_records.append(MetricRecord(
+                                'service.devnull.warning',
+                                'gauge',
+                                1))
+        expected_records.append(MetricRecord(
+                                'service.devnull.critical',
+                                'gauge',
+                                0))
+
+        expected_records.append(MetricRecord(
+                                'node.passing',
+                                'gauge',
+                                1))
+        expected_records.append(MetricRecord(
+                                'node.warning',
+                                'gauge',
+                                0))
+        expected_records.append(MetricRecord(
+                                'node.critical',
+                                'gauge',
+                                0))
+
+        actual_records = self.plugin._fetch_health_checks()
+
+        self.assertEqual(len(expected_records), len(actual_records))
+
+        for (idx, record) in enumerate(actual_records):
+            self._validate_single_record(expected_records[idx], record)
+
+    def _sample_response(self, path):
+
+        dir_name = os.path.dirname(os.path.realpath(__file__))
+        file = dir_name + '/resources' + path
+        with open(file, 'r') as data:
+            return json.load(data)
+
+    def _build_mock_consul_agent(self):
+
+        mock_agent = Mock()
+        api_host = 'example'
+        api_port = 8500
+        api_protocol = 'http'
+        acl_token = None
+        ssl_certs = {'ca_cert': None,
+                     'client_cert': None,
+                     'client_key': None}
+        agent = ConsulAgent(api_host,
+                            api_port,
+                            api_protocol,
+                            acl_token,
+                            ssl_certs)
+        agent.config = self._sample_response('/agent/self')
+
+        mock_agent.config = self._sample_response('/agent/self')
+        mock_agent.update_local_config = MagicMock()
+
+        with patch('consul_health_plugin.ConsulAgent.get_health_checks') as mcall:
+            mcall.return_value = self._sample_response('/health/node/mhost1.dummy.net')
+            h_map = agent.get_health_check_stats()
+            mock_agent.get_health_check_stats = MagicMock(return_value=h_map)
+
+        return mock_agent
+
+    def _validate_single_record(self, expected_record, actual_record):
+        self.assertIsNotNone(actual_record)
+        self.assertEqual(expected_record.name, actual_record.name)
+        self.assertEqual(expected_record.type, actual_record.type)
+        self.assertEqual(expected_record.value, actual_record.value)
+        self.assertIsNotNone(actual_record.timestamp)
